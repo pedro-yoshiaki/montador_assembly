4eff22deeb866ebe495bd7426048ee898a56cd0b
Pedro Yoshiaki <pedro.yoshiaki@gmail.com>
2024-11-20 20:25:37 -0300

    Update and rename Montador_Assembly_Versão3.c to main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*==============================================================*/
-                /*TÌtulo: Exemplo de Montador Assembly*/
+                /*T√≠tulo: Exemplo de Montador Assembly*/
                 /*Data: 20/08/2024*/
 /*==============================================================*/
 
@@ -9,12 +9,12 @@
 #include <string.h>
 #include <conio.h>
 
-/*Vari·veis Globais*/
+/*Vari√°veis Globais*/
 FILE  *  Arq;
 FILE  *  Trad;
-char     nome_arquivo[50+1]; /*Vari·vel Char com Null terminator para indicar final da string*/
+char     nome_arquivo[50+1]; /*Vari√°vel Char com Null terminator para indicar final da string*/
 char line [50+1]; /*Buffer para armazenar cada linha lida*/
-char save[10]; /* Buffer para o processamento da traduÁ„o, armazenando os endereÁos das instruÁıes*/
+char save[10]; /* Buffer para o processamento da tradu√ß√£o, armazenando os endere√ßos das instru√ß√µes*/
 
 /*Corpo do programa*/
 int main()
@@ -23,10 +23,10 @@ int main()
 	printf ("\nDigite o nome do arquivo: "); scanf ("%s", nome_arquivo); fflush (stdin);
 	printf ("\nEste foi o nome do arquivo digitado: %s\n", nome_arquivo); /*Linha para confirmar se capturou o nome correto*/
 	
-	/*A vari·vel Arq vai apontar para abrir o Arquivo capturado e Read(ler)*/
+	/*A vari√°vel Arq vai apontar para abrir o Arquivo capturado e Read(ler)*/
 	Arq = fopen (nome_arquivo , "r" );
 	
-	/*CondiÁ„o se o arquivo n„o existir*/
+	/*Condi√ß√£o se o arquivo n√£o existir*/
 	if ( Arq == NULL ) /* NULL = 0X000000000*/
 	{
 		printf ("\nIMPOSSIVEL ABRIR O ARQUIVO!");
@@ -45,9 +45,9 @@ int main()
 	
 	fprintf(Trad, "03 4E 44 52\n");
 	
-	/* L  o arquivo .ASM e transfere para o .mem */	
+	/* L√ä o arquivo .ASM e transfere para o .mem */	
 	while (fgets(line, sizeof(line), Arq) != NULL) /*Enquanto fgets ler uma linha do arquivo Arq e a armazenar no buffer line for diferente de NULL*/
-	/*sizeof garante que a linha ser· lida atÈ o tamanho da vari·vel line (50+1)*/
+	/*sizeof garante que a linha ser√° lida at√© o tamanho da vari√°vel line (50+1)*/
 	{
 		printf ("%s", line); /*Mostra na tela do programa as strings por linha pegos em line*/
 		if (strncmp(line, "NOP", 3) == 0) {
@@ -55,44 +55,44 @@ int main()
 	        }
 	        else if (strncmp(line, "STA", 3) == 0) {
 	            sscanf(line, "STA %s", save);
-	            fprintf(Trad, "10 00 %s 00\n", save);  /* STA: 10 00 <endereÁo> 00 */
+	            fprintf(Trad, "10 00 %s 00\n", save);  /* STA: 10 00 <endere√ßo> 00 */
 	        }
 	        else if (strncmp(line, "LDA", 3) == 0) {
 	            sscanf(line, "LDA %s", save);
-	            fprintf(Trad, "20 00 %s 00\n", save);  /* LDA: 20 00 <endereÁo> 00 */
+	            fprintf(Trad, "20 00 %s 00\n", save);  /* LDA: 20 00 <endere√ßo> 00 */
 	        }
 	        else if (strncmp(line, "ADD", 3) == 0) {
 	            sscanf(line, "ADD %s", save);
-	            fprintf(Trad, "30 00 %s 00\n", save);  /* ADD: 30 00 <endereÁo> 00 */
+	            fprintf(Trad, "30 00 %s 00\n", save);  /* ADD: 30 00 <endere√ßo> 00 */
 	        }
 	        else if (strncmp(line, "OR", 2) == 0) {
 	            sscanf(line, "OR %s", save);
-	            fprintf(Trad, "40 00 %s 00\n", save);  /* OR: 40 00 <endereÁo> 00 */
+	            fprintf(Trad, "40 00 %s 00\n", save);  /* OR: 40 00 <endere√ßo> 00 */
 	        }
 	        else if (strncmp(line, "AND", 3) == 0) {
 	            sscanf(line, "AND %s", save);
-	            fprintf(Trad, "50 00 %s 00\n", save);  /* AND: 50 00 <endereÁo> 00 */
+	            fprintf(Trad, "50 00 %s 00\n", save);  /* AND: 50 00 <endere√ßo> 00 */
 	        }
 	        else if (strncmp(line, "NOT", 3) == 0) {
 	            fprintf(Trad, "60 00 00 00\n");  /* NOT: 60 00 00 00 */
 	        }
 	        else if (strncmp(line, "JMP", 3) == 0) {
 	            sscanf(line, "JMP %s", save);
-	            fprintf(Trad, "80 00 %s 00\n", save);  /* JMP: 80 00 <endereÁo> 00 */
+	            fprintf(Trad, "80 00 %s 00\n", save);  /* JMP: 80 00 <endere√ßo> 00 */
 	        }
 	        else if (strncmp(line, "JN", 2) == 0) {
 	            sscanf(line, "JN %s", save);
-	            fprintf(Trad, "90 00 %s 00\n", save);  /* JN: 90 00 <endereÁo> 00 */
+	            fprintf(Trad, "90 00 %s 00\n", save);  /* JN: 90 00 <endere√ßo> 00 */
 	        }
 	        else if (strncmp(line, "JZ", 2) == 0) {
 	            sscanf(line, "JZ %s", save);
-	            fprintf(Trad, "A0 00 %s 00\n", save);  /* JZ: A0 00 <endereÁo> 00 */
+	            fprintf(Trad, "A0 00 %s 00\n", save);  /* JZ: A0 00 <endere√ßo> 00 */
 	        }
 	        else if (strncmp(line, "HLT", 3) == 0) {
 	            fprintf(Trad, "F0 00\n");  /* HLT: F0 00 00 00 */
 	        }
 	        else {
-	            printf("InstruÁ„o n„o reconhecida: %s\n", line);
+	            printf("Instru√ß√£o n√£o reconhecida: %s\n", line);
 	        }
 	   	  
 	}
